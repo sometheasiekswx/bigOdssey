cb1a3cf8e251f9828e08dc376dcc5b0d690fc2af
Somethea Siek <sometheasiekswx@gmail.com>
2025-03-09 12:54:17 +1100

    Update reverseKGroup1Space.py

--- python/reverseKGroup/reverseKGroup1Space.py
+++ python/reverseKGroup/reverseKGroup1Space.py
@@ -2,16 +2,46 @@
 
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     # Time: O(n)
-    # Space: O(1)
+    # Space O(1)
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        def getKth(node: Optional[ListNode]) -> Optional[ListNode]:
+            i = 0
+            while node and i < k:
+                node = node.next
+                i += 1
+            return node
+
         dummy = ListNode(0, head)
-        # TODO: Complete the code on my own
+        startNode = dummy
+        kNode = getKth(startNode)
+        while kNode:
+            previousNode = kNode.next
+            currentNode = startNode.next
+
+            while previousNode != kNode:
+                temp = currentNode.next
+                currentNode.next = previousNode
+                previousNode = currentNode
+                currentNode = temp
+
+            temp = startNode.next
+            startNode.next = kNode
+            startNode = temp
+            kNode = getKth(startNode)
+
         return dummy.next
